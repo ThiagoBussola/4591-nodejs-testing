d2ff2b90b17704eceb553b09bdcf2e53fd4ac63f
Thiago Bussola <thiagobussola@hotmail.com>
2025-04-01 14:09:12 -0300

    video-1.5

--- tests/scripts/test-generateBooks.js
+++ tests/scripts/test-generateBooks.js
@@ -34,3 +34,37 @@ test("Deve gerar o número correto de livros", async () => {
 
   await cleanupFile(testFilePath);
 });
+
+test("Deve gerar um NDJSON no formato valido", async () => {
+  const testFilePath = path.join(__dirname, "test-books.ndjson");
+  const numberOfBooks = 10;
+
+  await cleanupFile(testFilePath);
+  await generateBooksToNDJSON(testFilePath, numberOfBooks);
+
+  const content = await fs.readFile(testFilePath, "utf-8");
+  const lines = content.trim().split("\n");
+
+  assert.ok(
+    lines.every((line) => line.startsWith("{")),
+    "Cada linha deve ser um JSON"
+  );
+
+  lines.forEach((line, index) => {
+    assert.doesNotThrow(
+      () => JSON.parse(line),
+      `Linha ${index + 1} deve ser um JSON válido`
+    );
+  });
+
+  await cleanupFile(testFilePath);
+});
+
+test("Deve lançar um erro ao ler tentar ler um arquivo inexistente", async () => {
+  const invalidPath = "/dev/naoexiste/test-books.ndjson";
+
+  await assert.rejects(() => generateBooksToNDJSON(invalidPath, 10), {
+    name: "Error",
+    message: /ENOENT|EACCES/,
+  });
+});

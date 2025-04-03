d46f0ce113a7976fe25d11d76d03c1694b55c56b
Thiago Bussola <thiagobussola@hotmail.com>
2025-04-03 15:47:18 -0300

    video-5.1

---
+++ .jest/testEnv.js
@@ -0,0 +1,3 @@
+process.env.NODE_ENV = "dev";
+process.env.APP = "dev";
+process.env.JWT_SECRET = "batataDoce";
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+import { de } from "@faker-js/faker";
+
 export default {
   testMatch: ["**/tests/**/*.(test).[jt]s?(x)"],
+  setupFiles: ["./.jest/testEnv.js"],
   setupFilesAfterEnv: ["./tests/test-setup.js"],
   testEnvironment: "node",
   testTimeout: 30000,

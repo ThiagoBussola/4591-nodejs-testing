f597a766a54d837773f822e20a36ce63dfcd5d8f
Thiago Bussola <thiagobussola@hotmail.com>
2025-04-03 15:04:36 -0300

    video-4.4

--- tests/users/user.test.js
+++ tests/users/user.test.js
@@ -1,3 +1,4 @@
+import { beforeAll, describe, it, expect } from "@jest/globals";
 import { getTestServer, teardownTestServer } from "../test-setup.js";
 import User from "../../src/user/user.schema.js";
 import request from "supertest";
@@ -6,6 +7,7 @@ describe("Auth Controller", () => {
   let testUserData;
   let testServer;
   let authToken;
+  let userToUpdateId;
   const userNumber = Date.now();
 
   beforeAll(async () => {
@@ -18,6 +20,18 @@ describe("Auth Controller", () => {
     };
 
     await request(testServer).post("/auth/register").send(testUserData);
+
+    const userToUpdate = {
+      username: `usuarioUpdate`,
+      email: `usuarioupdate@teste.com`,
+      password: "teste1234",
+    };
+
+    const savedUser = await request(testServer)
+      .post("/auth/register")
+      .send(userToUpdate);
+
+    userToUpdateId = savedUser.body.id;
 
     const loginResponse = await request(testServer).post("/auth/login").send({
       email: testUserData.email,
@@ -36,8 +50,25 @@ describe("Auth Controller", () => {
         .get("/users")
         .set("Authorization", `Bearer ${authToken}`);
 
+      const countUsersOnDb = await User.countDocuments();
+
       expect(response.status).toBe(200);
       expect(response.body).toBeInstanceOf(Array);
+      expect(response.body.length).toBe(countUsersOnDb);
+    });
+  });
+
+  describe("PUT /users/:id", () => {
+    it("Deve atualizar um usuário por id", async () => {
+      const userToUpdate = await User.findById(userToUpdateId);
+
+      const response = await request(testServer)
+        .put(`/users/${userToUpdate._id}`)
+        .set("Authorization", `Bearer ${authToken}`)
+        .send({ username: "updatedUser" });
+
+      expect(response.status).toBe(200);
+      expect(response.body.username).toBe("updatedUser");
     });
   });
 });
